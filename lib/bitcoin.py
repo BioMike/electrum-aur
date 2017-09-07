21d98a6434ff067b92470caeb280c3aced4aa8cf
Myckel Habets <myckel@sdf.lonestar.org>
2017-09-07 07:19:58 +0200

    Auroracoin characteristics.

--- lib/bitcoin.py
+++ lib/bitcoin.py
@@ -42,7 +42,7 @@ DUST_THRESHOLD = 0
 DUST_SOFT_LIMIT = 100000
 MIN_RELAY_TX_FEE = 100000
 RECOMMENDED_FEE = 100000
-COINBASE_MATURITY = 100
+COINBASE_MATURITY = 80
 
 # AES encryption
 EncodeAES = lambda secret, s: base64.b64encode(aes.encryptData(secret,s))
@@ -241,7 +241,7 @@ def public_key_to_bc_address(public_key):
     h160 = hash_160(public_key)
     return hash_160_to_bc_address(h160)
 
-def hash_160_to_bc_address(h160, addrtype = 30):
+def hash_160_to_bc_address(h160, addrtype = 23):
     vh160 = chr(addrtype) + h160
     h = Hash(vh160)
     addr = vh160 + h[0:4]
@@ -415,7 +415,7 @@ from ecdsa.util import string_to_number, number_to_string
 def msg_magic(message):
     varint = var_int(len(message))
     encoded_varint = "".join([chr(int(varint[i:i+2], 16)) for i in xrange(0, len(varint), 2)])
-    return "\x1bDigibyte Signed Message:\n" + encoded_varint + message
+    return "\x1bAuroracoin Signed Message:\n" + encoded_varint + message
 
 
 def verify_message(address, signature, message):
